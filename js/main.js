f835548974e61ed0c4a4acca15eaf89ddb57ba13
GitEBF <ebfrappier@hotmail.ca>
2023-09-22 16:33:40 -0400

    aaaa

--- js/main.js
+++ js/main.js
@@ -2,25 +2,51 @@ const button = document.getElementById("loginButton");
 const sidebar = document.getElementById("loginSidebar");
 const buttonIcon = document.getElementById("buttonIcon");
 const buttonIconSetings = document.getElementById("buttonIconSetings");
-const test = document.getElementById("test");
-const happyEmoticon = document.getElementById("happyIcon");
-
-button.addEventListener("click", function() {
+const buttonWrapper = document.getElementById('buttonWrapper');
+button.addEventListener("click", function () {
   if (sidebar.style.width === "250px") {
     sidebar.style.width = "0";
-    button.style.left = "0"; 
-    buttonIcon.innerHTML = '<i class="fa fa-user"></i>'; 
-    buttonIconSetings.innerHTML = '<i class="fa fa-bars"></i>';
+    button.style.left = "0";
+    buttonIcon.innerHTML = '<i class="fa fa-user"></i>';
+    //buttonIconSetings.innerHTML = '<i class="fa fa-bars"></i>';
     button.classList.remove("active");
   } else {
     sidebar.style.width = "250px";
     button.style.left = "250px";
-    buttonIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#5C9EAD}</style><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>';
-    buttonIconSetings.innerHTML = '<i class="fa fa-bars"></i>';
+    buttonIcon.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" style="fill:#5C9EAD" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>';
+    //buttonIconSetings.innerHTML = '<i class="fa fa-bars"></i>';
     button.classList.add("active");
   }
 });
-
-
+/*
+for (i=0;i<emotionIcons.length;i++) {
+  emotionIcons[i].addEventListener("click", emotionClick(emotionIcons[i]));
 };
 
+Array.from(emotionIcons).forEach((icon) =>
+{
+  var divMessage = document.getElementById("msg");
+  divMessage.innerHTML = icon.id;
+});
+
+
+function emotionClick(emotionButton) {
+  console.log(emotionButton.id);
+}
+*/
+
+buttonWrapper.addEventListener('click', (event) => {
+  var emotionIcons = document.getElementsByClassName("emotionIcon");
+  console.dir(event.target.id);
+  if (event.target.id != null && event.target.id != "buttonWrapper") {
+    for (i = 0; i < emotionIcons.length; i++) {
+      if (emotionIcons[i].id != event.target.id) {
+        emotionIcons[i].classList.add("emotionDisapear");
+      } else {  
+        emotionIcons[i].classList.add("emotionBiggify");
+      }
+    }
+
+
+  }
+})
