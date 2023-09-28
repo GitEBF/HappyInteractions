d1476d209122f1a5c6c5e736c57d9df81ce20864
tijaycegep <barackobama.amidelaforet@gmail.com>
2023-09-28 17:03:30 -0400

    enlever style svg

--- settings.php
+++ settings.php
@@ -87,8 +87,7 @@ require "dbController.php";
         }
 
         ?>
-        <a href="index.php"><svg xmlns="http://www.w3.org/2000/svg" height="2em"
-                viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+        <a href="index.php"><svg xmlns="http://www.w3.org/2000/svg" style="fill:#00000" height="2em" viewBox="0 0 512 512">
 
                 <path
                     d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z" />
@@ -163,14 +162,15 @@ require "dbController.php";
                             </p>
                         </div>
                     </label>
-                    <a href="modification.php?id=<?php echo $row["id"] ?>"><svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                    <a href="modification.php?id=<?php echo $row["id"] ?>"><svg xmlns="http://www.w3.org/2000/svg"
+                            style="fill:#00000" height="1em"
                             viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
 
                             <path
                                 d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z" />
                         </svg></a>
                     <a href="zoom.php?id=<?php echo $row["id"] ?>">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                        <svg xmlns="http://www.w3.org/2000/svg" style="fill:#00000" height="1em"
                             viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
                             <path
                                 d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
